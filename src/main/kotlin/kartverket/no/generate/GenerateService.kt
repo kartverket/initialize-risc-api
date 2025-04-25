d30cc28bdcc0c1e031bcaaa2be7acfac373f8bad
annaaurora <anna-aurora@live.no>
2025-04-25 12:57:48 +0200

    fjernet kode som har noe med sopsConfig-endepunktet å gjøre

--- src/main/kotlin/kartverket/no/generate/GenerateService.kt
+++ src/main/kotlin/kartverket/no/generate/GenerateService.kt
@@ -2,18 +2,9 @@ package kartverket.no.generate
 
 import kartverket.no.airTable.AirTableClientService
 import kartverket.no.config.AppConfig
-import kartverket.no.generate.model.CreationRule
 import kartverket.no.generate.model.DefaultInitialRiSc
-import kartverket.no.generate.model.GcpCryptoKeyObject
-import kartverket.no.generate.model.KeyGroup
-import kartverket.no.generate.model.PublicAgeKey
-import kartverket.no.generate.model.ResourceId
 import kartverket.no.generate.model.RiScContent
-import kartverket.no.generate.model.SopsConfig
-import kartverket.no.generate.model.toResourceId
 import kartverket.no.securityMetrics.SecurityMetricsResponse
-import kartverket.no.utils.YamlUtils
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 object GenerateService {
@@ -28,39 +19,6 @@ object GenerateService {
             AirTableClientService.fetchDefaultRiSc(),
             initialRiScContent,
         ).content
-
-    fun getSopsConfig(
-        gcpCryptoKey: GcpCryptoKeyObject,
-        publicAgeKeys: List<PublicAgeKey>,
-    ) = YamlUtils
-        .mapToYamlString(
-            SopsConfig(
-                creationRules =
-                    listOf(
-                        CreationRule(
-                            pathRegex = config.pathRegex,
-                            shamirThreshold = 2,
-                            keyGroups =
-                                listOfNotNull(
-                                    KeyGroup(
-                                        age = listOf(config.securityTeamPublicKey),
-                                        gcpKms = listOf(ResourceId(gcpCryptoKey.toResourceId())),
-                                    ),
-                                    KeyGroup(
-                                        age = listOf(config.backendPublicKey, config.securityPlatformPublicKey),
-                                    ),
-                                    if (publicAgeKeys.isNotEmpty()) {
-                                        KeyGroup(
-                                            age = publicAgeKeys.map { it.value },
-                                        )
-                                    } else {
-                                        null
-                                    },
-                                ),
-                        ),
-                    ),
-            ),
-        ).removePrefix("---\n")
 
     private fun generateInitialRiScContent(
         securityMetrics: SecurityMetricsResponse?,
--- src/main/kotlin/kartverket/no/generate/model/DTOs.kt
+++ src/main/kotlin/kartverket/no/generate/model/DTOs.kt
@@ -8,18 +8,3 @@ import kotlinx.serialization.Serializable
 data class GenerateRiScRequestBody(
     val initialRiSc: String,
 )
-
-@Serializable
-data class GenerateSopsConfigRequestBody(
-    val gcpCryptoKey: GcpCryptoKeyObject,
-    val publicAgeKeys: List<PublicAgeKey>,
-)
-
-@Serializable
-data class GcpCryptoKeyObject(
-    val projectId: GcpProjectId,
-    val keyRing: String,
-    val name: String,
-)
-
-fun GcpCryptoKeyObject.toResourceId() = "projects/${projectId.value}/locations/europe-north1/keyRings/$keyRing/cryptoKeys/$name"
