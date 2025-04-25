15f718842e622daaab099236573683b92b144607
annaaurora <anna-aurora@live.no>
2025-04-25 14:52:05 +0200

    fjernet kode som ikke brukes og  har med sikkerhetsmetrikkelen å gjøre

--- src/main/kotlin/kartverket/no/generate/GenerateService.kt
+++ src/main/kotlin/kartverket/no/generate/GenerateService.kt
@@ -1,24 +1,18 @@
 package kartverket.no.generate
 
 import kartverket.no.airTable.AirTableClientService
-import kartverket.no.config.AppConfig
 import kartverket.no.generate.model.DefaultInitialRiSc
 import kartverket.no.generate.model.RiScContent
-import kartverket.no.securityMetrics.SecurityMetricsResponse
 import kotlinx.serialization.json.Json
 
 object GenerateService {
-    private val config = AppConfig.generateRiScConfig
-
     suspend fun generateDefaultRiSc(initialRiScContent: RiScContent): String =
         generateInitialRiScContent(
-            null,
             AirTableClientService.fetchDefaultRiSc(),
             initialRiScContent,
         ).content
 
     private fun generateInitialRiScContent(
-        securityMetrics: SecurityMetricsResponse?,
         defaultRiSc: RiScContent,
         initialRiScContent: RiScContent,
     ): DefaultInitialRiSc =
